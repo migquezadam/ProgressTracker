a186ab65856b507c66dfafbb582f76f9c88cd7d0
Miguel Quezada <mig.aquezada@gmail.com>
2022-06-21 21:12:17 -0400

    Initial Commit

---
+++ ProgressTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ProgressTracker
+//
+//  Created by Miguel Quezada on 21-06-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ProgressTracker/ProgressTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressTrackerApp.swift
+//  ProgressTracker
+//
+//  Created by Miguel Quezada on 21-06-22.
+//
+
+import SwiftUI
+
+@main
+struct ProgressTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
