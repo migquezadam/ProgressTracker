9b1586540adce94d06f38dbdf8e8f220acfc5890
Miguel Quezada <mig.aquezada@gmail.com>
2022-06-22 17:29:53 -0400

    add basic ui

---
+++ ProgressTracker/HomeView.swift
@@ -0,0 +1,30 @@
+//
+//  HomeView.swift
+//  ProgressTracker
+//
+//  Created by Miguel Quezada on 22-06-22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @EnvironmentObject var dataController: DataController
+
+    var body: some View {
+        NavigationView {
+            VStack {
+                Button("Add Data") {
+                    dataController.deleteAll()
+                    try? dataController.createSampleData()
+                }
+            }
+            .navigationTitle("Home")
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
