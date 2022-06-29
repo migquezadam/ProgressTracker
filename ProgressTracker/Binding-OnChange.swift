dfc80570f2a0feda1a2199a0bb7126793a2318da
Miguel Quezada <mig.aquezada@gmail.com>
2022-06-29 04:58:57 -0400

    add save to changes

---
+++ ProgressTracker/Binding-OnChange.swift
@@ -0,0 +1,20 @@
+//
+//  Binding-OnChange.swift
+//  ProgressTracker
+//
+//  Created by Miguel Quezada on 29-06-22.
+//
+
+import SwiftUI
+
+extension Binding {
+    func onChange(_ handler: @escaping () -> Void) -> Binding<Value> {
+        Binding(
+            get: { self.wrappedValue },
+            set: { newValue in
+                self.wrappedValue = newValue
+                handler()
+            }
+        )
+    }
+}
--- ProgressTracker/ProgressTrackerApp.swift
+++ ProgressTracker/ProgressTrackerApp.swift
@@ -23,7 +23,13 @@ struct ProgressTrackerApp: App {
             ContentView()
                 .environment(\.managedObjectContext, dataController.container.viewContext)
                 .environmentObject(dataController)
+                .onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification), perform: save)
+
 
         }
     }
+    func save(_ note: Notification) {
+        dataController.save()
+    }
+
 }
