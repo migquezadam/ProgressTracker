e9f4a376d1997007f458503b730a8d27fe895783
Miguel Quezada <mig.aquezada@gmail.com>
2022-06-29 04:23:12 -0400

    add edit view

---
+++ ProgressTracker/ItemRowView.swift
@@ -0,0 +1,24 @@
+//
+//  ItemRowView.swift
+//  ProgressTracker
+//
+//  Created by Miguel Quezada on 29-06-22.
+//
+
+import SwiftUI
+
+struct ItemRowView: View {
+    @ObservedObject var item: Item
+
+    var body: some View {
+        NavigationLink(destination: EditItemView(item: item)) {
+            Text(item.itemTitle)
+        }
+    }
+}
+
+struct ItemRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemRowView(item: Item.example)
+    }
+}
